550226445b3dd3eee05845ce79e9eca7f247801a
Aman jii <151424799+Botsthe@users.noreply.github.com>
2023-12-26 20:29:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Botsthe/TG_BOTZ.git /TG_BOTZ
+  git clone https://github.com/Botsthe/PremiumPfilterbot /PremiumPfilterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TG_BOTZ
+  git clone $UPSTREAM_REPO /PremiumPfilterbot
 fi
-cd /TG_BOTZ
+cd /PremiumPfilterbot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
