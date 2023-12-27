b3351e7d722917ad0be7f28c10eb57976d30fd60
Aman jii <151424799+Botsthe@users.noreply.github.com>
2023-12-27 18:15:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Botsthe/PremiumPfilterbot /PremiumPfilterbot
+  git clone https://github.com/Botsthe/PremiumPfilterbot.git /PremiumPfilterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PremiumPfilterbot
